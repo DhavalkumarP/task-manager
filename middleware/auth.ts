8de53259380b8947f8286113f89ccf6804a2c182
Dhaval Panchal <panchaldhaval049@gmail.com>
2025-07-31 12:03:59 +0530

    Update auth.ts

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,6 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
 import { verifyToken, extractTokenFromHeader } from "@/lib/jwt";
-import { ICommonResponse } from "@/services/types";
+import { ICommonResponse } from "@/types/common";
 
 export interface AuthenticatedRequest extends NextRequest {
   user?: {
